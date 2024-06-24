a3b67e3b5779c964456f8b8faa467fada7f12e1f
Andrea Fan <shan32157@gmail.com>
2024-06-24 14:09:13 +0800

    Complete building form and post contact to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 // Vite env: https://vitejs.dev/guide/env-and-mode
 
 import { useState, useEffect } from "react";
+import ContactTable from "./components/contact-table/contact-table";
+import ContactForm from "./components/contact-form/contact-form";
 
-import "./App.css";
-
-type ContactType = {
+export type ContactType = {
   firstName: string;
   lastName: string;
   email: string;
-}[];
+};
 
 function App() {
-  const [contacts, setContacts] = useState<ContactType>([]);
+  const [contacts, setContacts] = useState<ContactType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const CONTACTS = import.meta.env.VITE_GET_CONTACTS as string;
+  const GET_CONTACTS = import.meta.env.VITE_GET_CONTACTS as string;
 
   async function fetchContacts() {
     try {
       setIsLoading(true);
-      const res = await fetch(CONTACTS);
+      const res = await fetch(GET_CONTACTS);
 
       if (res.status === 200) {
         const data = await res.json();
@@ -39,8 +39,9 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex w-full max-w-[1400px] mx-auto gap-5 items-baseline">
+      {isLoading ? <span>Loading</span> : <ContactTable contacts={contacts} />}
+      <ContactForm />
     </div>
   );
 }
---
+++ frontend/src/components/contact-form/contact-form.tsx
@@ -0,0 +1,113 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "../../components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../components/ui/form";
+import { Input } from "../../components/ui/input";
+
+const formSchema = z.object({
+  firstName: z.string().min(2, {
+    message: "First Name must be at least 2 characters.",
+  }),
+  lastName: z.string().min(2, {
+    message: "Last Name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+});
+
+function ContactForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const POST_CONTACT = import.meta.env.VITE_CREATE_CONTACT as string;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    };
+
+    try {
+      const res = await fetch(POST_CONTACT, options);
+      if (res.status !== 200 && res.status !== 201) {
+        const data = await res.json();
+        console.log(data);
+        alert(data.message);
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        alert(e.message);
+      }
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-[40%] border p-5 flex flex-col gap-3 rounded-md shadow-md"
+      >
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>First Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter you first name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter you last name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter you email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default ContactForm;
